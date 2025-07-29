ff83f33397d5c4186e1752b18af4de5c0083aa03
Mem Robin Çen <memrobincen@gmail.com>
2025-07-29 20:24:09 +0300

    refactor: remove unused imports in SellerEntity class

--- src/main/java/org/robn/ecommerce/seller/model/entity/SellerEntity.java
+++ src/main/java/org/robn/ecommerce/seller/model/entity/SellerEntity.java
@@ -2,11 +2,8 @@ package org.robn.ecommerce.seller.model.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.robn.ecommerce.address.model.entity.AddressEntity;
 import org.robn.ecommerce.seller.model.enums.SellerStatus;
 import org.robn.ecommerce.user.model.entity.UserEntity;
-
-import java.util.List;
 
 @Entity
 @Getter
