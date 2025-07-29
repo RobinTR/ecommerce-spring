6016d6b0e07563ee23b30c530b68f0fc443aff1e
Mem Robin Çen <memrobincen@gmail.com>
2025-07-29 19:27:25 +0300

    feat: add AddressEntity class

---
+++ src/main/java/org/robn/ecommerce/address/model/entity/AddressEntity.java
@@ -0,0 +1,47 @@
+package org.robn.ecommerce.address.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.robn.ecommerce.common.model.entity.BaseEntity;
+import org.robn.ecommerce.customer.model.entity.CustomerEntity;
+import org.robn.ecommerce.seller.model.entity.SellerEntity;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "eco_address", indexes = {
+        @Index(name = "idx_address_city", columnList = "city"),
+        @Index(name = "idx_address_city_district", columnList = "city, district"),
+        @Index(name = "idx_address_city_district_neighborhood", columnList = "city, district, neighborhood")
+})
+public class AddressEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "district")
+    private String district;
+
+    @Column(name = "neighborhood")
+    private String neighborhood;
+
+    @Column(name = "address", length = 500)
+    private String address;
+
+    @Column(name = "is_default")
+    private Boolean isDefault;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", insertable = false, updatable = false)
+    private CustomerEntity customer;
+}
