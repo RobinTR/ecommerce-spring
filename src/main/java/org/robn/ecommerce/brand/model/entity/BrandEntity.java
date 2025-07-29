850981e93e6c9c7305ad118c5da43a708211475b
Mem Robin Çen <memrobincen@gmail.com>
2025-07-29 20:20:29 +0300

    feat: change id generation strategy in BrandEntity to UUID

--- src/main/java/org/robn/ecommerce/brand/model/entity/BrandEntity.java
+++ src/main/java/org/robn/ecommerce/brand/model/entity/BrandEntity.java
@@ -6,6 +6,7 @@ import org.robn.ecommerce.common.model.entity.BaseEntity;
 import org.robn.ecommerce.product.model.entity.ProductEntity;
 
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -16,9 +17,9 @@ import java.util.List;
 @Table(name = "eco_brand")
 public class BrandEntity extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id")
-    private Long id;
+    private UUID id;
 
     @Column(name = "name")
     private String name;
