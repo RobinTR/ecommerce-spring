1644ac2e184c5b96043d2dff73a3db2a15b1a1da
Mem Robin Çen <memrobincen@gmail.com>
2025-07-29 20:21:13 +0300

    feat: add ProductImageEntity class to manage product images with UUID as ID

---
+++ src/main/java/org/robn/ecommerce/product/model/entity/ProductImageEntity.java
@@ -0,0 +1,34 @@
+package org.robn.ecommerce.product.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.robn.ecommerce.common.model.entity.BaseEntity;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "eco_product_image")
+public class ProductImageEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "product_id")
+    private UUID productId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", insertable = false, updatable = false)
+    private ProductEntity product;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "alt_text")
+    private String altText;
+}
