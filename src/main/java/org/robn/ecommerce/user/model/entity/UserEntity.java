a3ed8c96b51d3752c665e5cc0e439a189c0e1aca
Mem Robin Çen <memrobincen@gmail.com>
2025-07-29 20:23:09 +0300

    feat: add UserEntity class as a base for user-related entities with UUID as ID

---
+++ src/main/java/org/robn/ecommerce/user/model/entity/UserEntity.java
@@ -0,0 +1,24 @@
+package org.robn.ecommerce.user.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
