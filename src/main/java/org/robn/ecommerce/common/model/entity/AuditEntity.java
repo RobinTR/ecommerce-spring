aa28169516787d1f11ee1e9ef226a0b2ad0b5b7c
Mem Robin Çen <memrobincen@gmail.com>
2025-08-02 14:26:18 +0300

    feat: add AuditEntity to track createdBy and updatedBy fields

---
+++ src/main/java/org/robn/ecommerce/common/model/entity/AuditEntity.java
@@ -0,0 +1,22 @@
+package org.robn.ecommerce.common.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class AuditEntity extends BaseEntity {
+
+    //TODO: Refactor later to implement Spring Security
+    @Column(name = "created_by", updatable = false)
+    protected String createdBy;
+
+    @Column(name = "updated_by")
+    protected String updatedBy;
+
+}
