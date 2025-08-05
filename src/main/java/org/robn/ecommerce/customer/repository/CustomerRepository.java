85fb86df26957d15e592934c9a0b5db95e34e890
Mem Robin Çen <memrobincen@gmail.com>
2025-08-05 16:09:32 +0300

    feat: add CustomerRepository for managing CustomerEntity

---
+++ src/main/java/org/robn/ecommerce/customer/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package org.robn.ecommerce.customer.repository;
+
+import org.robn.ecommerce.customer.model.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CustomerRepository extends JpaRepository<CustomerEntity, UUID> {
+}
