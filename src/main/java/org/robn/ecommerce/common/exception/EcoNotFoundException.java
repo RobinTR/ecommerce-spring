d579c2d659cf78bdda663f38b2f9192880ef763b
Mem Robin Çen <memrobincen@gmail.com>
2025-08-06 17:02:30 +0300

    refactor: make EcoNotFoundException abstract and enhance documentation

--- src/main/java/org/robn/ecommerce/common/exception/EcoNotFoundException.java
+++ src/main/java/org/robn/ecommerce/common/exception/EcoNotFoundException.java
@@ -2,12 +2,20 @@ package org.robn.ecommerce.common.exception;
 
 import java.io.Serial;
 
-public class EcoNotFoundException extends RuntimeException {
+/**
+ * Base class for exceptions that indicate a resource was not found.
+ */
+public abstract class EcoNotFoundException extends RuntimeException {
 
     @Serial
     private static final long serialVersionUID = 7564942602822378516L;
 
-    public EcoNotFoundException(String message) {
+    /**
+     * Constructs a new {@link EcoNotFoundException} with the specified detail message.
+     *
+     * @param message the detail message
+     */
+    protected EcoNotFoundException(final String message) {
         super(message);
     }
 
