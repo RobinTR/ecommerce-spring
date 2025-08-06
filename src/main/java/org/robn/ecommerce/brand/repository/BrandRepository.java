688dd23e3849dd30c1e9adaa86813ee7170665cc
Mem Robin Çen <memrobincen@gmail.com>
2025-08-06 17:02:40 +0300

    feat: add method to find BrandEntity by name in BrandRepository

--- src/main/java/org/robn/ecommerce/brand/repository/BrandRepository.java
+++ src/main/java/org/robn/ecommerce/brand/repository/BrandRepository.java
@@ -3,5 +3,10 @@ package org.robn.ecommerce.brand.repository;
 import org.robn.ecommerce.brand.model.entity.BrandEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BrandRepository extends JpaRepository<BrandEntity, Long> {
+
+    Optional<BrandEntity> findByName(String name);
+
 }
