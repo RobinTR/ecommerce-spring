49026e8f62aa349c2cdc0671f9d40e1e3e9fa115
Mem Robin Çen <memrobincen@gmail.com>
2025-08-07 15:44:41 +0300

    refactor: update EcoBaseResponse and EcoErrorResponse

--- src/main/java/org/robn/ecommerce/common/model/response/EcoBaseResponse.java
+++ src/main/java/org/robn/ecommerce/common/model/response/EcoBaseResponse.java
@@ -3,18 +3,14 @@ package org.robn.ecommerce.common.model.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Builder
+@SuperBuilder
 public class EcoBaseResponse<T> {
-
-    public static final EcoBaseResponse<Void> SUCCESS = EcoBaseResponse.<Void>builder()
-            .httpStatus(HttpStatus.OK)
-            .isSuccess(true)
-            .build();
 
     @Builder.Default
     private LocalDateTime timestamp = LocalDateTime.now();
@@ -25,6 +21,13 @@ public class EcoBaseResponse<T> {
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T response;
+
+    public static <T> EcoBaseResponse<T> success() {
+        return EcoBaseResponse.<T>builder()
+                .httpStatus(HttpStatus.OK)
+                .isSuccess(true)
+                .build();
+    }
 
     public static <T> EcoBaseResponse<T> successOf(final T response) {
         return EcoBaseResponse.<T>builder()
--- src/main/java/org/robn/ecommerce/common/model/response/EcoErrorResponse.java
+++ src/main/java/org/robn/ecommerce/common/model/response/EcoErrorResponse.java
@@ -1,17 +1,19 @@
 package org.robn.ecommerce.common.model.response;
 
+import lombok.experimental.SuperBuilder;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
+@SuperBuilder
 public class EcoErrorResponse extends EcoBaseResponse<String> {
 
-    public EcoErrorResponse(HttpStatus httpStatus, String message) {
-        super(LocalDateTime.now(), httpStatus, false, message);
-    }
-
     public static EcoErrorResponse failureOf(HttpStatus httpStatus, String message) {
-        return new EcoErrorResponse(httpStatus, message);
+        return EcoErrorResponse.builder()
+                .httpStatus(httpStatus)
+                .isSuccess(false)
+                .response(message)
+                .build();
     }
 
 }
