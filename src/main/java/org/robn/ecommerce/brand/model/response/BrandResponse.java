40326655ecd4b287a3ff0107f0f3038983e99707
Mem Robin Çen <memrobincen@gmail.com>
2025-08-07 23:28:53 +0300

    feat: add BrandResponse class for brand data representation

---
+++ src/main/java/org/robn/ecommerce/brand/model/response/BrandResponse.java
@@ -0,0 +1,13 @@
+package org.robn.ecommerce.brand.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BrandResponse {
+
+    private Long id;
+    private String name;
+
+}
