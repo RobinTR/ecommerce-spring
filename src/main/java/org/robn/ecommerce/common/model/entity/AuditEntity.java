85d6b22d04b0f8bbb380c9b7cbcdc132fe0692da
Mem Robin Çen <memrobincen@gmail.com>
2025-08-08 19:08:03 +0300

    feat: implement audit entity listener to set default createdBy and updatedBy values

--- src/main/java/org/robn/ecommerce/common/model/entity/AuditEntity.java
+++ src/main/java/org/robn/ecommerce/common/model/entity/AuditEntity.java
@@ -1,15 +1,20 @@
 package org.robn.ecommerce.common.model.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.robn.ecommerce.common.listener.AuditEntityListener;
 
 @Getter
+@Setter
 @SuperBuilder
 @NoArgsConstructor
 @MappedSuperclass
+@EntityListeners(AuditEntityListener.class)
 public abstract class AuditEntity extends BaseEntity {
 
     @Column(name = "created_by", updatable = false)
