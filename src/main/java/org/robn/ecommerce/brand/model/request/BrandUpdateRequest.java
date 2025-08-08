42b1aa1c27f904ac90f923c00aede461bffda7be
Mem Robin Çen <memrobincen@gmail.com>
2025-08-08 19:09:53 +0300

    feat: update name validation to use NameWithNumber annotation in request models

--- src/main/java/org/robn/ecommerce/brand/model/request/BrandUpdateRequest.java
+++ src/main/java/org/robn/ecommerce/brand/model/request/BrandUpdateRequest.java
@@ -1,18 +1,16 @@
 package org.robn.ecommerce.brand.model.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import org.robn.ecommerce.common.util.validation.Name;
+import org.robn.ecommerce.common.util.validation.name.NameWithNumber;
 
 @Getter
 @Setter
 public class BrandUpdateRequest {
 
-    @Name
+    @NameWithNumber
     @NotBlank
     @Size(min = 1, max = 100)
     private String name;
