d35bce2e0bbfac123e7aa223a4b95b45484dcd45
Mem Robin Çen <memrobincen@gmail.com>
2025-08-09 22:22:53 +0300

    feat: add request and response models for category management

---
+++ src/main/java/org/robn/ecommerce/category/model/request/CategoryUpdateRequest.java
@@ -0,0 +1,18 @@
+package org.robn.ecommerce.category.model.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.robn.ecommerce.common.util.validation.name.Name;
+
+@Getter
+@Setter
+public class CategoryUpdateRequest {
+
+    @Name
+    @NotBlank
+    @Size(min = 1, max = 100)
+    private String name;
+
+}
---
+++ src/main/java/org/robn/ecommerce/category/model/response/CategoryResponse.java
@@ -0,0 +1,13 @@
+package org.robn.ecommerce.category.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryResponse {
+
+    private Long id;
+    private String name;
+
+}
