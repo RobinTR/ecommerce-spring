a27cbd1fb8e7a132035a802cd1af07c71c3de5d6
Mem Robin Çen <memrobincen@gmail.com>
2025-08-09 20:56:46 +0300

    refactor: add createdBy and updatedBy fields to BaseEntity with default values

--- src/main/java/org/robn/ecommerce/common/model/entity/BaseEntity.java
+++ src/main/java/org/robn/ecommerce/common/model/entity/BaseEntity.java
@@ -23,19 +23,33 @@ public abstract class BaseEntity {
     @Column(name = "updated_at")
     protected LocalDateTime updatedAt;
 
+    @Column(name = "created_by", updatable = false)
+    protected String createdBy;
+
+    @Column(name = "updated_by")
+    protected String updatedBy;
+
+
+    /*
+     * TODO: Refactor for Spring Security - Jwt
+     */
     @PrePersist
     public void prePersist() {
-        LocalDateTime now = LocalDateTime.now();
 
         this.createdAt = Optional.ofNullable(this.createdAt)
-                .orElse(now);
+                .orElse(LocalDateTime.now());
 
-        this.updatedAt = now;
+        this.createdBy = Optional.ofNullable(this.createdBy)
+                .orElse("eco");
+
     }
 
     @PreUpdate
     public void preUpdate() {
+
         this.updatedAt = LocalDateTime.now();
+        this.updatedBy = "eco";
+
     }
 
 }
