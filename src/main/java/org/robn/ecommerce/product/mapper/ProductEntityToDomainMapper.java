e04239b8667f726d1461c80bae3524df604e4f48
Mem Robin Çen <memrobincen@gmail.com>
2025-08-10 15:30:24 +0300

    feat: add mappers for converting between Product and ProductEntity

---
+++ src/main/java/org/robn/ecommerce/product/mapper/ProductEntityToDomainMapper.java
@@ -0,0 +1,10 @@
+package org.robn.ecommerce.product.mapper;
+
+import org.mapstruct.Mapper;
+import org.robn.ecommerce.common.mapper.BaseMapper;
+import org.robn.ecommerce.product.model.Product;
+import org.robn.ecommerce.product.model.entity.ProductEntity;
+
+@Mapper(componentModel = "spring")
+public interface ProductEntityToDomainMapper extends BaseMapper<ProductEntity, Product> {
+}
