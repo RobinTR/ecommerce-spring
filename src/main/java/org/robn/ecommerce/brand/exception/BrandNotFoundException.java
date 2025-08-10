2733d134f06ec0325975c4fba239673e8d1debda
Mem Robin Çen <memrobincen@gmail.com>
2025-08-10 18:18:42 +0300

    refactor: rename BrandNotExistException to BrandNotFoundException for clarity

--- src/main/java/org/robn/ecommerce/brand/exception/BrandNotFoundException.java
+++ src/main/java/org/robn/ecommerce/brand/exception/BrandNotFoundException.java
@@ -4,18 +4,18 @@ import org.robn.ecommerce.common.exception.EcoNotFoundException;
 
 import java.io.Serial;
 
-public final class BrandNotExistException extends EcoNotFoundException {
+public final class BrandNotFoundException extends EcoNotFoundException {
 
     @Serial
     private static final long serialVersionUID = 2529033075050356615L;
 
     /**
-     * Constructs a new {@link BrandNotExistException} with the specified detail message.
+     * Constructs a new {@link BrandNotFoundException} with the specified detail message.
      *
      * @param id the ID of the brand that does not exist
      */
-    public BrandNotExistException(final Long id) {
-        super("Brand with id: " + id + " does not exist.");
+    public BrandNotFoundException(final Long id) {
+        super(String.format("Brand with id %d was not found.", id));
     }
 
 }
