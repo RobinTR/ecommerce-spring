c7d59b5eefa9f6320fbbf3b8316ab75f3bf38274
Mem Robin Çen <memrobincen@gmail.com>
2025-08-10 15:26:29 +0300

    refactor: remove EqualsAndHashCode annotation from Brand class

--- src/main/java/org/robn/ecommerce/brand/model/Brand.java
+++ src/main/java/org/robn/ecommerce/brand/model/Brand.java
@@ -1,6 +1,5 @@
 package org.robn.ecommerce.brand.model;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
@@ -9,7 +8,6 @@ import org.robn.ecommerce.common.model.BaseDomainModel;
 @Getter
 @Setter
 @SuperBuilder
-@EqualsAndHashCode(callSuper = true)
 public class Brand extends BaseDomainModel {
 
     private Long id;
