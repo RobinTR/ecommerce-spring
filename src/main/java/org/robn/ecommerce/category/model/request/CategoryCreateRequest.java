715f16174b9a5efba9900c37124a7ae0419b2fde
Mem Robin Çen <memrobincen@gmail.com>
2025-08-14 20:43:49 +0300

    refactor: update name validation annotation in CategoryCreateRequest

--- src/main/java/org/robn/ecommerce/category/model/request/CategoryCreateRequest.java
+++ src/main/java/org/robn/ecommerce/category/model/request/CategoryCreateRequest.java
@@ -4,13 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import org.robn.ecommerce.common.util.validation.name.Name;
+import org.robn.ecommerce.common.util.validation.name.NameWithNumber;
 
 @Getter
 @Setter
 public class CategoryCreateRequest {
 
-    @Name
+    @NameWithNumber
     @NotBlank
     @Size(min = 1, max = 100)
     private String name;
