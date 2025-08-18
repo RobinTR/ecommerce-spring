3aa81b313ee5a902ecfdf1ee43eb5ddfdf0f919f
Mem Robin Çen <memrobincen@gmail.com>
2025-08-18 16:19:31 +0300

    feat: add exception handling for InventoryAlreadyExistsException and improve error response formatting

--- src/main/java/org/robn/ecommerce/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/org/robn/ecommerce/common/exception/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package org.robn.ecommerce.common.exception.handler;
 import jakarta.validation.ConstraintViolationException;
 import org.robn.ecommerce.common.exception.EcoNotFoundException;
 import org.robn.ecommerce.common.model.response.EcoErrorResponse;
+import org.robn.ecommerce.inventory.exception.InventoryAlreadyExistsException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,39 +20,50 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EcoNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<EcoErrorResponse> handleEcoNotFoundException(final EcoNotFoundException exception) {
-        EcoErrorResponse ecoErrorResponse = EcoErrorResponse.failureOf(HttpStatus.NOT_FOUND, exception.getMessage());
+        final EcoErrorResponse ecoErrorResponse = EcoErrorResponse.failureOf(HttpStatus.NOT_FOUND, exception.getMessage());
+
+        return new ResponseEntity<>(ecoErrorResponse, ecoErrorResponse.getHttpStatus());
+    }
+
+    @ExceptionHandler(InventoryAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<EcoErrorResponse> handleInventoryAlreadyExistsException(final InventoryAlreadyExistsException exception) {
+        final EcoErrorResponse ecoErrorResponse = EcoErrorResponse.failureOf(HttpStatus.CONFLICT, exception.getMessage());
+
         return new ResponseEntity<>(ecoErrorResponse, ecoErrorResponse.getHttpStatus());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<EcoErrorResponse> handleValidationException(final MethodArgumentNotValidException exception) {
-        String errorMessage = exception.getBindingResult()
+        final String errorMessage = exception.getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .map(error -> error.getField() + ": " + error.getDefaultMessage())
                 .collect(Collectors.joining(", "));
 
-        EcoErrorResponse ecoErrorResponse = EcoErrorResponse.failureOf(HttpStatus.BAD_REQUEST, errorMessage);
+        final EcoErrorResponse ecoErrorResponse = EcoErrorResponse.failureOf(HttpStatus.BAD_REQUEST, errorMessage);
+
         return new ResponseEntity<>(ecoErrorResponse, ecoErrorResponse.getHttpStatus());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<EcoErrorResponse> handleConstraintViolationException(final ConstraintViolationException exception) {
-        String errorMessage = exception.getConstraintViolations()
+        final String errorMessage = exception.getConstraintViolations()
                 .stream()
                 .map(violation -> violation.getPropertyPath() + ": " + violation.getMessage())
                 .collect(Collectors.joining(", "));
 
-        EcoErrorResponse ecoErrorResponse = EcoErrorResponse.failureOf(HttpStatus.BAD_REQUEST, errorMessage);
+        final EcoErrorResponse ecoErrorResponse = EcoErrorResponse.failureOf(HttpStatus.BAD_REQUEST, errorMessage);
+
         return new ResponseEntity<>(ecoErrorResponse, ecoErrorResponse.getHttpStatus());
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<EcoErrorResponse> handleDataIntegrityViolationException(final DataIntegrityViolationException exception) {
-        EcoErrorResponse ecoErrorResponse = EcoErrorResponse.failureOf(
+        final EcoErrorResponse ecoErrorResponse = EcoErrorResponse.failureOf(
                 HttpStatus.BAD_REQUEST,
                 "Invalid request: referential integrity check failed. Please verify related fields."
         );
