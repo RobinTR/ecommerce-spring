dfdab38a799c67b94fbde6648128834c165d626c
Mem Robin Çen <memrobincen@gmail.com>
2025-08-22 21:26:21 +0300

    feat: add security configuration and user repository method for email lookup

---
+++ src/main/java/org/robn/ecommerce/auth/config/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package org.robn.ecommerce.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(customizer -> customizer
+                        .requestMatchers(HttpMethod.GET, "/api/v1/customer-addresses").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return httpSecurity.build();
+    }
+
+    /** Returns a new instance of {@link BCryptPasswordEncoder} as the password encoder.
+     * This bean is used to encode and verify passwords in the application.
+     *
+     * @return A {@link PasswordEncoder} instance using BCrypt hashing algorithm.
+     */
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- src/main/java/org/robn/ecommerce/auth/repository/UserRepository.java
+++ src/main/java/org/robn/ecommerce/auth/repository/UserRepository.java
@@ -3,7 +3,11 @@ package org.robn.ecommerce.auth.repository;
 import org.robn.ecommerce.auth.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    Optional<UserEntity> findByEmail(String email);
+
 }
