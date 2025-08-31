4185bc294fc06eb1fa8d630145344edf271ebe12
Mem Robin Çen <memrobincen@gmail.com>
2025-08-31 14:32:57 +0300

    feat: enhance access control in BrandController and update security configuration

--- src/main/java/org/robn/ecommerce/auth/config/SecurityConfiguration.java
+++ src/main/java/org/robn/ecommerce/auth/config/SecurityConfiguration.java
@@ -33,6 +33,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/api/v1/guest-addresses/**").permitAll()
+                        .requestMatchers("/api/v1/brands/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
--- src/main/java/org/robn/ecommerce/brand/controller/BrandController.java
+++ src/main/java/org/robn/ecommerce/brand/controller/BrandController.java
@@ -11,6 +11,7 @@ import org.robn.ecommerce.brand.model.response.BrandListResponse;
 import org.robn.ecommerce.brand.model.response.BrandResponse;
 import org.robn.ecommerce.brand.service.BrandService;
 import org.robn.ecommerce.common.model.response.EcoBaseResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,29 +27,31 @@ public class BrandController {
 
     @GetMapping
     public EcoBaseResponse<List<BrandListResponse>> findAll() {
-        List<Brand> brands = brandService.findAll();
-        List<BrandListResponse> brandsResponse = brandDomainToListResponseMapper.map(brands);
+        final List<Brand> brands = brandService.findAll();
+        final List<BrandListResponse> brandsResponse = brandDomainToListResponseMapper.map(brands);
 
         return EcoBaseResponse.successOf(brandsResponse);
     }
 
     @GetMapping("/{id}")
-    public EcoBaseResponse<BrandResponse> findById(@PathVariable Long id) {
-        Brand brand = brandService.findById(id);
-        BrandResponse brandResponse = brandDomainToResponseMapper.map(brand);
+    public EcoBaseResponse<BrandResponse> findById(@PathVariable final Long id) {
+        final Brand brand = brandService.findById(id);
+        final BrandResponse brandResponse = brandDomainToResponseMapper.map(brand);
 
         return EcoBaseResponse.successOf(brandResponse);
     }
 
     @PostMapping
-    public EcoBaseResponse<Void> create(@RequestBody @Valid BrandCreateRequest brandCreateRequest) {
+    @PreAuthorize("hasRole('ADMIN')")
+    public EcoBaseResponse<Void> create(@RequestBody @Valid final BrandCreateRequest brandCreateRequest) {
         brandService.create(brandCreateRequest);
 
         return EcoBaseResponse.success();
     }
 
     @PutMapping("/{id}")
-    public EcoBaseResponse<Void> update(@PathVariable Long id, @RequestBody @Valid BrandUpdateRequest brandUpdateRequest) {
+    @PreAuthorize("hasRole('ADMIN')")
+    public EcoBaseResponse<Void> update(@PathVariable final Long id, @RequestBody @Valid final BrandUpdateRequest brandUpdateRequest) {
         brandService.update(id, brandUpdateRequest);
 
         return EcoBaseResponse.success();
