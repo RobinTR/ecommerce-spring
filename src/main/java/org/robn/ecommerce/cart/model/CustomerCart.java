bf72fa1414a34d6d134009b43f35941ba1a081a1
Mem Robin Çen <memrobincen@gmail.com>
2025-09-07 14:54:52 +0300

    refactor: remove unused imports and clean up CustomerCart class for improved clarity

--- src/main/java/org/robn/ecommerce/cart/model/CustomerCart.java
+++ src/main/java/org/robn/ecommerce/cart/model/CustomerCart.java
@@ -3,10 +3,7 @@ package org.robn.ecommerce.cart.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.robn.ecommerce.cart.model.enums.CartStatus;
-import org.robn.ecommerce.common.model.BaseDomainModel;
 
-import java.math.BigDecimal;
 import java.util.UUID;
 
 @Getter
