4b921d746c902aed9cc9c97f48198f99b1c7688f
Mem Robin Çen <memrobincen@gmail.com>
2025-09-08 19:19:35 +0300

    refactor: rename EcoTokenInvalidException to EcoInvalidTokenException for clarity

--- src/main/java/org/robn/ecommerce/auth/exception/EcoInvalidTokenException.java
+++ src/main/java/org/robn/ecommerce/auth/exception/EcoInvalidTokenException.java
@@ -6,7 +6,7 @@ import java.io.Serial;
  * Exception thrown when a token is not valid.
  * This exception extends {@link EcoAuthException} and provides static factory methods for creating instances.
  */
-public class EcoTokenInvalidException extends EcoAuthException {
+public class EcoInvalidTokenException extends EcoAuthException {
 
     @Serial
     private static final long serialVersionUID = 3610043563580453033L;
@@ -16,7 +16,7 @@ public class EcoTokenInvalidException extends EcoAuthException {
      *
      * @param jwt   The JWT (JSON Web Token) that is not valid.
      */
-    private EcoTokenInvalidException(String jwt) {
+    private EcoInvalidTokenException(String jwt) {
         super(String.format("Token is not valid! token: %s", jwt));
     }
 
@@ -26,29 +26,29 @@ public class EcoTokenInvalidException extends EcoAuthException {
      * @param jwt   The JWT (JSON Web Token) that is not valid.
      * @param cause The cause of the exception.
      */
-    private EcoTokenInvalidException(String jwt, Throwable cause) {
+    private EcoInvalidTokenException(String jwt, Throwable cause) {
         super(String.format("Token is not valid! token: %s", jwt), cause);
     }
 
     /**
-     * Static factory method to create an instance of {@link EcoTokenInvalidException} with the specified JWT.
+     * Static factory method to create an instance of {@link EcoInvalidTokenException} with the specified JWT.
      *
      * @param jwt The JWT (JSON Web Token) that is not valid.
-     * @return A new instance of {@link EcoTokenInvalidException}.
+     * @return A new instance of {@link EcoInvalidTokenException}.
      */
-    public static EcoTokenInvalidException of(final String jwt) {
-        return new EcoTokenInvalidException(jwt);
+    public static EcoInvalidTokenException of(final String jwt) {
+        return new EcoInvalidTokenException(jwt);
     }
 
     /**
-     * Static factory method to create an instance of {@link EcoTokenInvalidException} with the specified JWT and cause.
+     * Static factory method to create an instance of {@link EcoInvalidTokenException} with the specified JWT and cause.
      *
      * @param jwt   The JWT (JSON Web Token) that is not valid.
      * @param cause The cause of the exception.
-     * @return A new instance of {@link EcoTokenInvalidException}.
+     * @return A new instance of {@link EcoInvalidTokenException}.
      */
-    public static EcoTokenInvalidException of(final String jwt, final Throwable cause) {
-        return new EcoTokenInvalidException(jwt, cause);
+    public static EcoInvalidTokenException of(final String jwt, final Throwable cause) {
+        return new EcoInvalidTokenException(jwt, cause);
     }
 
 }
