e845834cabda50b642b9c1680f796bff681f0161
Mem Robin Çen <memrobincen@gmail.com>
2025-09-13 16:17:18 +0300

    refactor: remove unused imports in CustomerAddress and SellerAddress classes for cleaner code

--- src/main/java/org/robn/ecommerce/address/model/CustomerAddress.java
+++ src/main/java/org/robn/ecommerce/address/model/CustomerAddress.java
@@ -1,10 +1,8 @@
 package org.robn.ecommerce.address.model;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.robn.ecommerce.common.model.BaseDomainModel;
 
 import java.util.UUID;
 
--- src/main/java/org/robn/ecommerce/address/model/SellerAddress.java
+++ src/main/java/org/robn/ecommerce/address/model/SellerAddress.java
@@ -3,7 +3,6 @@ package org.robn.ecommerce.address.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.robn.ecommerce.common.model.BaseDomainModel;
 
 import java.util.UUID;
 
