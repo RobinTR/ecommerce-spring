447771f6a3250830e8b606ddf30fa5393cb11944
Mem Robin Çen <memrobincen@gmail.com>
2025-09-22 15:06:37 +0300

    feat: add repositories for CartSnapshot and CartItemSnapshot entities

---
+++ src/main/java/org/robn/ecommerce/cart/repository/CartItemSnapshotRepository.java
@@ -0,0 +1,9 @@
+package org.robn.ecommerce.cart.repository;
+
+import org.robn.ecommerce.cart.model.entity.CartItemSnapshotEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CartItemSnapshotRepository extends JpaRepository<CartItemSnapshotEntity, UUID> {
+}
---
+++ src/main/java/org/robn/ecommerce/cart/repository/CartSnapshotRepository.java
@@ -0,0 +1,9 @@
+package org.robn.ecommerce.cart.repository;
+
+import org.robn.ecommerce.cart.model.entity.CartSnapshotEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CartSnapshotRepository extends JpaRepository<CartSnapshotEntity, UUID> {
+}
