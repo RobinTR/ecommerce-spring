9ff42363437d9f65e01cf1fb7edca553c3346ad0
Mem Robin Çen <memrobincen@gmail.com>
2025-10-17 22:41:35 +0300

    refactor: simplify ProductCategory to store only IDs

--- src/main/java/org/robn/ecommerce/productcategory/model/ProductCategory.java
+++ src/main/java/org/robn/ecommerce/productcategory/model/ProductCategory.java
@@ -3,9 +3,7 @@ package org.robn.ecommerce.productcategory.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.robn.ecommerce.category.model.Category;
 import org.robn.ecommerce.common.model.BaseDomainModel;
-import org.robn.ecommerce.product.model.Product;
 
 @Getter
 @Setter
@@ -14,8 +12,6 @@ public class ProductCategory extends BaseDomainModel {
 
     private Long id;
     private Long productId;
-    private Product product;
     private Long categoryId;
-    private Category category;
 
 }
